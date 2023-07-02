df607c4520a917caad74b0d7534fcf46bce259ee
Shaelie Raby <shaelieraby@yahoo.com>
2023-07-02 16:27:39 -0500

    rendered-api

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React from "react";
+//import { handlers } from "../mocks/handlers";
+
+const Character = (props) => {
+ const { character } = props
+    return (
+        <div className="character-data-wrapper">
+            <h2>{character.name}</h2>
+            <h3 className="character-info-wrapper">More Information</h3>
+                <ul className="character-info-list">
+                    <li>Height: {character.height}</li>
+                    <li>Mass: {character.mass}</li>
+                    <li>Hair Color: {character.hair_color}</li> 
+                    <li>Skin Color: {character.skin_color}</li> 
+                    <li>Eye Color: {character.eye_color}</li> 
+                    <li>Birth Year: {character.birth_year}</li> 
+                    <li>Gender: {character.gender}</li> 
+                </ul>
+        </div>
+    )
+}
+
+export default Character;
